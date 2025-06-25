35bef5923bbfde82eef49a20d3a386056261400f
mc303 <daniel@buonocore.nl>
2025-06-25 21:18:13 +0000

    25 Jun 2025, 23:18

---
+++ src/resources/contracts.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+    List, Datagrid, TextField, ReferenceField, EditButton, DeleteButton,
+    Edit, SimpleForm, TextInput, ReferenceInput, SelectInput, Create, DateInput
+} from 'react-admin';
+
+export const ContractList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <TextField source="start_date" />
+            <TextField source="end_date" />
+            <ReferenceField source="department_id" reference="departments" link="show">
+                <TextField source="name" />
+            </ReferenceField>
+            <EditButton />
+            <DeleteButton />
+        </Datagrid>
+    </List>
+);
+
+export const ContractEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput source="id" disabled />
+            <TextInput source="name" />
+            <DateInput source="start_date" />
+            <DateInput source="end_date" />
+            <ReferenceInput source="department_id" reference="departments">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+        </SimpleForm>
+    </Edit>
+);
+
+export const ContractCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+            <DateInput source="start_date" />
+            <DateInput source="end_date" />
+            <ReferenceInput source="department_id" reference="departments">
+                <SelectInput optionText="name" />
+            </ReferenceInput>
+        </SimpleForm>
+    </Create>
+);
---
+++ src/resources/departments.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {
+    List, Datagrid, TextField, EditButton, DeleteButton,
+    Edit, SimpleForm, TextInput, Create
+} from 'react-admin';
+
+export const DepartmentList = props => (
+    <List {...props}>
+        <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="name" />
+            <EditButton />
+            <DeleteButton />
+        </Datagrid>
+    </List>
+);
+
+export const DepartmentEdit = props => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput source="id" disabled />
+            <TextInput source="name" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const DepartmentCreate = props => (
+    <Create {...props}>
+        <SimpleForm>
+            <TextInput source="name" />
+        </SimpleForm>
+    </Create>
+);
